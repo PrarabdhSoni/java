06724a8ce51ebfd63471d486e8b1f8cfb98c17c4
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-15 11:22:02 +0530

    Adding Equal Sum Checker program

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,18 @@
+public class EqualSumChecker {
+
+        public static boolean hasEqualSum(int num1, int num2, int num3){
+            if(num1 + num2 == num3){
+                return true;
+            }else if (num1 + num3 == num2){
+                return true;
+            }else if(num2 + num3 == num1){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public static void main(String[] args){
+            System.out.println(hasEqualSum(1, 1, 2));
+        }
+}
