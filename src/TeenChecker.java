e60d094f1d9bfc2f64a8e07f325df8a20ad718d4
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-15 21:37:28 +0530

    Adding Teen Number Checker program

---
+++ src/TeenChecker.java
@@ -0,0 +1,24 @@
+public class TeenChecker {
+        public static boolean hasTeen(int num1, int num2, int num3){
+
+            if(num1>=13 && num1 <=19){
+                return true;
+            }else if (num2 >= 13 && num2 <= 19)
+            {
+                return true;
+            }else if(num3 >= 13 && num3 <= 19){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        public static boolean isTeen(int num){
+            return num >= 13 && num <= 19;
+        }
+
+    public static void main(String[] args){
+        System.out.println(hasTeen(4,18,27));
+        System.out.println(isTeen(15));
+    }
+}
