e14fbcfb790b596b415fa60baa6d69dce5f5762a
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-16 11:17:06 +0530

    Adding Metrics Conversions program

---
+++ src/MetricConversions.java
@@ -0,0 +1,15 @@
+public class MetricConversions {
+    public static double convertToCentimeters(int heightInch){
+        return heightInch * 2.54;
+    }
+
+    public static double convertToCentimeters(int heightFeet, int heightInches){
+        int totalHeightInches = (heightFeet * 12) + heightInches;
+        return convertToCentimeters(totalHeightInches);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToCentimeters(5,8));
+        System.out.println(convertToCentimeters(68));
+    }
+}
