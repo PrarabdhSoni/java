d2eb96d286713e4f8d76cd47f71873a95f64d808
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-18 10:26:34 +0530

    Adding Seconds and minutes challege program

---
+++ src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,33 @@
+public class SecondsAndMinutesChallenge {
+
+    public static void main(String[] args) {
+
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
+    }
+
+    public static String getDurationString(int seconds){
+
+        if (seconds < 0) {
+            return "Invalid Seconds" + seconds;
+        }
+
+        return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds){
+
+        if(minutes <0 ){
+            return "Invalid Minutes" + minutes;
+        }
+
+        if (seconds <0 || seconds >59){
+            return "Invalid data for seconds";
+        }
+
+        int hour = minutes /60;
+        int remainingMinutes = minutes % 60;
+
+        return hour +"h " + remainingMinutes + "m " + seconds + "s ";
+    }
+}
