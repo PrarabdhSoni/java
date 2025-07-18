b9cd65193a7f549974e7236ea9812b63f74d5d3a
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-18 10:50:42 +0530

    Adding minutes to years and days program

--- src/MinutesToYearsDaysCalculator.java
+++ src/MinutesToYearsDaysCalculator.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class MinutesToYearsDaysCalculator {
+    // write code here
+
+    public static void printYearsAndDays(long minutes){
+
+        if (minutes <0){
+            System.out.println("Invalid Value");
+        }else{
+
+            long hours = minutes / 60;
+            long day = hours /24;
+            long year = day/365;
+            long remaningDays = day % 365;
+
+            System.out.println( minutes +" min = " + year + " y and " + remaningDays + " d");
+        }
+    }
+
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
 }
