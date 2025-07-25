53c69d711a410a445fba598b066726d07f9b14b7
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-07-25 09:28:17 +0530

    Adding numbers in word for switch statement practice

---
+++ src/ControlStatements/WordRepresentationOfNumbers.java
@@ -0,0 +1,33 @@
+package ControlStatements;
+
+public class WordRepresentationOfNumbers {
+        // write code here
+
+        public static void printNumberInWord(int num){
+
+            String numberInWord;
+
+            switch(num){
+                case 0 -> numberInWord = "ZERO";
+                case 1 -> numberInWord = "ONE";
+                case 2 -> numberInWord = "TWO";
+                case 3 -> numberInWord = "THREE";
+                case 4 -> numberInWord = "FOUR";
+                case 5 -> numberInWord = "FIVE";
+                case 6 -> numberInWord = "SIX";
+                case 7 -> numberInWord = "SEVEN";
+                case 8 -> numberInWord = "EIGHT";
+                case 9 -> numberInWord = "NINE";
+                default -> numberInWord = "OTHER";
+            };
+
+            System.out.println(numberInWord);
+        }
+
+    public static void main(String[] args) {
+
+            printNumberInWord(2);
+            printNumberInWord(0);
+            printNumberInWord(100);
+    }
+}
